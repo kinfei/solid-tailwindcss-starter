207bbb6e97ce868930e1088df2a3949de860925f
kinfei <kinfei1230@gmail.com>
2022-05-30 00:20:50 +0800

    Revert "fix typo"
    This reverts commit 70c0eac3450e8ab9a8aeb388c996952b87944b94.

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
         <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
           <div class="px-4 py-6 sm:px-0">
             <div class="border-4 border-dashed border-gray-200 rounded-lg h-96 p-4 text-center text-gray-400">
-              Content for about page
+              Content for home page
             </div>
           </div>
         </div>
