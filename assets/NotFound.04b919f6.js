a3dd78c0fa6c5705013473870452ed731a5b229b
kinfei <kinfei@users.noreply.github.com>
2022-07-03 13:54:39 +0000

    deploy: d25ddd3443737cc660cc236c3e7f02f08f9e0574

---
+++ assets/NotFound.04b919f6.js
@@ -0,0 +1,1 @@
+import{H as o}from"./Header.d43a8d46.js";import{c as t}from"./index.596b1841.js";const a=()=>t(o,{title:"Not found"});export{a as default};
--- assets/NotFound.7ee70422.js
+++
@@ -1,1 +0,0 @@
-import{H as o}from"./Header.f820c7a1.js";import{c as t}from"./index.d3b354d1.js";const a=()=>t(o,{title:"Not found"});export{a as default};
