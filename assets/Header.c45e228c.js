690ce00cd550e614b4a20cc07285430440ddb562
kinfei <kinfei@users.noreply.github.com>
2022-07-10 00:44:14 +0000

    deploy: d36e99ef51b0b873446dc354f2dafed66d4a30d2

---
+++ assets/Header.c45e228c.js
@@ -0,0 +1,1 @@
+import{i as a,t as r}from"./index.6be9dc8c.js";const i=r('<header class="bg-white shadow"><div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8"><h1 class="text-3xl font-bold leading-tight text-gray-900"></h1></div></header>'),d=e=>(()=>{const t=i.cloneNode(!0),l=t.firstChild,s=l.firstChild;return a(s,()=>e.title),t})();export{d as H};
